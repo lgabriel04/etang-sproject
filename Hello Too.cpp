0ef42c7ab28dce8eb8a8b4046acabe314d2b7a95
lgabriel04 <110146300+lgabriel04@users.noreply.github.com>
2022-07-30 23:02:24 +0800

    ADD
    new

---
+++ Hello Too.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string greetings = "Hello World";
+	string full_name = "Rich Aleeza C. Campos";
+	
+	cout << greetings << endl;
+	cout << full_name;
+	
+	return 0;
+}
